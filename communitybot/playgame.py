dd1ce2c0f7531320cbce48356aa778184acecfbf
DerAsmo <der_asmo@gmx.de>
2018-02-16 11:30:55 +0100

    add validation for votes

--- communitybot/playgame.py
+++ communitybot/playgame.py
@@ -10,7 +10,7 @@ class Game:
         self.s = steemd_instance
         self.commit = Commit(steemd_instance=self.s)
         self.bot_account = bot_account
-        self.debug = true
+        self.debug = bool(1)
 
     def get_sample_comment(self, user):
         data = {'derasmo' :  'vorstellung-von-mir-und-meiner-projektidee', 'flurgx' : 're-derasmo-vorstellung-von-mir-und-meiner-projektidee-20180213t210718018z', 'kurodevs' : 're-derasmo-vorstellung-von-mir-und-meiner-projektidee-20180213t203220853z'}
@@ -20,12 +20,25 @@ class Game:
 
         return bool(0)
 
+    def get_valid_votes(self, votes, limit):
+        v_votes = {}
+        s_votes = sorted(votes, key=lambda tmp: tmp['time'])
+
+        if self.debug:
+            for vote in s_votes:
+                if self.get_sample_comment(vote['voter']) != bool(0):
+                    v_votes.append(vote)
+        else:
+            v_votes = s_votes
+
+        return itertools.islice(s_votes, limit)
+
     def build_permlink(self):
         return str(uuid.uuid4())
 
     def post(self, title, body, author, reply_identifier=None):
         if self.debug:
-            permlink = get_sample_comment(author)
+            permlink = self.get_sample_comment(author)
         else:
             permlink = self.build_permlink()
             self.commit.post(title, body, author, permlink, reply_identifier)
@@ -71,14 +84,14 @@ class Game:
     
                     for vote in s_votes:
                         if vote['voter'] not in voters:
-                            voters.append(vote['voter'])
 
-                            if self.debug and self.get_sample_comment(vote['voter']) != 0:
-                                comment_permlink = self.post('', body, vote['voter'], postid)
+                            if self.debug and self.get_sample_comment(vote['voter']) != bool(0):
+                                voters.append(vote['voter'])
+                                permlinks[vote['voter']] = self.post('', body, vote['voter'], postid)
                             elif self.debug == bool(0):
-                                comment_permlink = self.post('', body, self.bot_account, postid)
+                                voters.append(vote['voter'])
+                                permlinks[vote['voter']] = self.post('', body, self.bot_account, postid)
 
-                            permlinks[vote['voter']] = comment_permlink
             time.sleep(5)
 
         # 3. post summary
