cf861d783f9771af791685edeea224a4fe79fa5d
DerAsmo <der_asmo@gmx.de>
2018-02-16 09:50:25 +0100

    bugfixes

--- communitybot/playgame.py
+++ communitybot/playgame.py
@@ -1,3 +1,4 @@
+import itertools
 import time
 import uuid
 
@@ -5,55 +6,86 @@ from steem.commit import Commit
 
 class Game:
 
-    def __init__(self, steemd_instance, expected_tags, bot_account):
+    def __init__(self, steemd_instance, bot_account):
         self.s = steemd_instance
         self.commit = Commit(steemd_instance=self.s)
         self.bot_account = bot_account
+        self.debug = true
+        self.sample = 'flurgx'
+
+    def get_sample_comment(self, user):
+        data = {'flurgx' : 're-derasmo-vorstellung-von-mir-und-meiner-projektidee-20180213t210718018z', 'kurodevs' : 're-derasmo-vorstellung-von-mir-und-meiner-projektidee-20180213t203220853z'}
+        
+        return data[user]
 
     def build_permlink(self):
         return str(uuid.uuid4())
 
     def post(self, title, body, author, reply_identifier=None):
-        permlink = self.build_permlink();
-    
-        self.commit.post(title, body, author, permlink, reply_identifier)
+        if self.debug:
+            permlink = get_sample_comment(self.sample)
+        else:
+            permlink = self.build_permlink()
+            self.commit.post(title, body, author, permlink, reply_identifier)
 
         return permlink;
 
-    def start_game(selfs):
+    def evaluate(self, results):
+        output = ''
+
+        for voter, result in results.items():
+            oLine = '<div>' + voter + ': ' + str(result) + '</div>'
+            print(oLine)
+            output = output + oLine
+
+        return output
+
+    def start_game(self):
 
         # 1. create post
-        title = "test title"
-        body = "test body content"
+        title = 'test title'
+        body = 'test body'
         permlink = self.post(title, body, self.bot_account)
 
         # 2. catch upvotes and create comments
         postid = "@" + self.bot_account + "/" + permlink;
         start = time.time()
-        hours = 24
+        hours = 0
         minutes = 0
-        seconds = 0
+        seconds = 15
         duration = hours * 360 + minutes * 60 + seconds
         
         limit = 2;
         voters = []
-        permlinks = []
+        permlinks ={}
 
         while time.time() < start + duration:
-            votes = s.get_active_votes(self.bot_account, permlink);
+            if len(voters) < limit :
+                votes = self.s.get_active_votes(self.bot_account, permlink)
 
-            if voters.count() > 0 and voters.count() < limit :
-                for vote in votes:
-                    if vote['voter'] not in voters:
-                        voters.append(vote['voter'])
-                        permlinks[vote['voter']] = self.post('', body, author, postid)
-            time.sleep(10)
+                if len(votes) > 0 :
+                    # sort votes by time and cut off above limit
+                    s_votes = itertools.islice(sorted(votes, key=lambda tmp: tmp['time']), limit)
+    
+                    for vote in s_votes:
+                        if vote['voter'] not in voters:
+                            voters.append(vote['voter'])
+
+                            if self.debug:
+                                self.sample = vote['voter']
+
+                            permlinks[vote['voter']] = self.post('', body, self.bot_account, postid)
+            time.sleep(5)
 
         # 3. post summary
-        results = [];
-        for key, voter in voters:
-            votes = s.get_active_votes(self.bot_account, permlinks[voter])
-            results[voter] = votes.count()
+        results = {}
+        for voter in voters:
+            if self.debug:
+                votes = self.s.get_active_votes(voter, permlinks[voter])
+            else:
+                votes = self.s.get_active_votes(self.bot_account, permlinks[voter])
 
-        bodyResult = evaluate(results)
+            results[voter] = len(votes)
+
+        bodyResult = self.evaluate(results)
         self.post('', bodyResult, self.bot_account, postid)
